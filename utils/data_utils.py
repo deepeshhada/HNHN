03a5ac7b7e29cf0b81746dbc297a32410057e0c9
Deepesh Hada <deepesh.hada@gmail.com>
2021-03-12 17:18:07 +0530

    added utils to save datasets at fixed seed

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import torch
 import torch.nn as nn
+import random
 import math
 import numpy.linalg as linalg
 import matplotlib.pyplot as plt
@@ -13,23 +14,145 @@ import pickle
 import warnings
 import sklearn.metrics
 
-
 res_dir = 'results'
 data_dir = 'data'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def save_embeddings(ids, out_dim, mode="user"):
-    embeddings = nn.Embedding(len(ids), out_dim)
+class HNHNData(object):
+    """
+        Construct Dataset for HNHN
+    """
+
+    def __init__(self, args, ratings):
+        random.seed(args.seed)
+        self.args = args
+        self.ratings = ratings
+        self.num_ng = args.num_ng
+        self.num_ng_test = args.num_ng_test
+        self.batch_size = args.test_batch_size
+
+        self.preprocess_ratings = self._reindex(self.ratings)
+
+        self.user_pool = set(self.ratings['user_id'].unique())
+        self.item_pool = set(self.ratings['item_id'].unique())
+
+        self.train_ratings, self.test_ratings = self._leave_one_out(self.preprocess_ratings)
+        self.negatives = self._negative_sampling(self.preprocess_ratings)
+
+    def _reindex(self, ratings):
+        """
+            Process dataset to reindex userID and itemID, also set rating as binary feedback
+        """
+        user_list = list(ratings['user_id'].drop_duplicates())
+        user2id = {w: i for i, w in enumerate(user_list)}
+
+        item_list = list(ratings['item_id'].drop_duplicates())
+        item2id = {w: i for i, w in enumerate(item_list)}
+
+        ratings['user_id'] = ratings['user_id'].apply(lambda x: user2id[x])
+        ratings['item_id'] = ratings['item_id'].apply(lambda x: item2id[x])
+        ratings['rating'] = ratings['rating'].apply(lambda x: float(x > 0))
+        return ratings
+
+    def _leave_one_out(self, ratings):
+        ratings['rank_latest'] = ratings.groupby(['user_id'])['timestamp'].rank(method='first', ascending=False)
+        test = ratings.loc[ratings['rank_latest'] == 1]
+        train = ratings.loc[ratings['rank_latest'] > 1]
+        assert train['user_id'].nunique() == test['user_id'].nunique(), 'Not Match Train User with Test User'
+        return train[['user_id', 'item_id', 'rating']], test[['user_id', 'item_id', 'rating']]
+
+    def _negative_sampling(self, ratings):
+        interact_status = (
+            ratings.groupby('user_id')['item_id']
+                .apply(set)
+                .reset_index()
+                .rename(columns={'item_id': 'interacted_items'}))
+        interact_status['negative_items'] = interact_status['interacted_items'].apply(lambda x: self.item_pool - x)
+        interact_status['negative_samples'] = interact_status['negative_items'].apply(
+            lambda x: random.sample(x, self.num_ng_test))
+        return interact_status[['user_id', 'negative_items', 'negative_samples']]
+
+    def save_train_instance(self):
+        save_path = os.path.join('data', self.args.dataset_name, 'train.csv')
+        users, items, ratings = [], [], []
+        train_ratings = pd.merge(self.train_ratings, self.negatives[['user_id', 'negative_items']], on='user_id')
+        train_ratings['negatives'] = train_ratings['negative_items'].apply(lambda x: random.sample(x, self.num_ng))
+        for row in train_ratings.itertuples():
+            users.append(int(row.user_id))
+            items.append(int(row.item_id))
+            ratings.append(float(row.rating))
+            for i in range(self.num_ng):
+                users.append(int(row.user_id))
+                items.append(int(row.negatives[i]))
+                ratings.append(float(0))  # negative samples get 0 rating
+        dataset = RatingDataset(
+            user_list=users,
+            item_list=items,
+            rating_list=ratings)
+        df = pd.DataFrame(columns=('users', 'items', 'ratings'))
+        df['users'], df['items'], df['ratings'] = dataset.user_list, dataset.item_list, dataset.rating_list
+        df.to_csv(save_path, sep='\t', index=False, header=False)
+        return df
+
+    def save_test_instance(self):
+        save_path = os.path.join('data', self.args.dataset_name, 'test.csv')
+        users, items, ratings = [], [], []
+        test_ratings = pd.merge(self.test_ratings, self.negatives[['user_id', 'negative_samples']], on='user_id')
+        for row in test_ratings.itertuples():
+            users.append(int(row.user_id))
+            items.append(int(row.item_id))
+            ratings.append(float(row.rating))
+            for i in getattr(row, 'negative_samples'):
+                users.append(int(row.user_id))
+                items.append(int(i))
+                ratings.append(float(0))
+        dataset = RatingDataset(
+            user_list=users,
+            item_list=items,
+            rating_list=ratings)
+        df = pd.DataFrame(columns=('users', 'items', 'ratings'))
+        df['users'], df['items'], df['ratings'] = dataset.user_list, dataset.item_list, dataset.rating_list
+        df.to_csv(save_path, sep='\t', index=False, header=False)
+        return df
+
+
+class RatingDataset(torch.utils.data.Dataset):
+    def __init__(self, user_list, item_list, rating_list):
+        super(RatingDataset, self).__init__()
+        self.user_list = user_list
+        self.item_list = item_list
+        self.rating_list = rating_list
+
+    def __len__(self):
+        return len(self.user_list)
+
+    def __getitem__(self, idx):
+        user = self.user_list[idx]
+        item = self.item_list[idx]
+        rating = self.rating_list[idx]
+
+        return (
+            torch.tensor(user, dtype=torch.long),
+            torch.tensor(item, dtype=torch.long),
+            torch.tensor(rating, dtype=torch.float)
+        )
+
+
+def save_embeddings(args, ids, mode):
+    save_path = os.path.join('data', args.dataset_name, mode + '_embeddings.pkl')
+    embeddings = nn.Embedding(len(ids), args.embed_dim)
     embed_dict = dict()
+
     for idx, node_id in enumerate(ids):
         embed_dict[node_id] = embeddings(torch.LongTensor([idx])).detach().squeeze().numpy()
 
-    with open('./data/generic/' + mode + '_embeddings.pkl', 'wb') as f:
+    with open(save_path, 'wb') as f:
         pickle.dump(embed_dict, f, pickle.HIGHEST_PROTOCOL)
 
 
-def read_embeddings(mode="user"):
-    with open('./data/generic/' + mode + '_embeddings.pkl', 'rb') as f:
+def read_embeddings(args, mode):
+    load_path = os.path.join('data', args.dataset_name, mode + '_embeddings.pkl')
+    with open(load_path, 'rb') as f:
         embed_dict = pickle.load(f)
     return embed_dict
